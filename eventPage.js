6a69315f4b75d7cfd4370ebf9620f14c864f9b30
Igor Justino <dadah89@gmail.com>
2013-04-02 02:53:23 -0700

    Make background script non persistent (loaded only when it's needed)

--- eventPage.js
+++ eventPage.js
@@ -1,14 +1,13 @@
 function openTicketInZendesk(tab) {
-  console.log('tab before match', tab);
   var match = tab.url.match(/^https?:\/\/(.*)\.zendesk.com\/tickets\/(\d+)$/);
+
   if (match) {
     var domain  = match[1],
         pattern = '*://' + domain + '.zendesk.com/agent/*',
         id      = match[2];
-    // console.log('domain', domain, 'pattern', pattern);
+
     chrome.tabs.query({ url: pattern }, function(tabs) {
       var lotusTab = tabs[0];
-      // console.log('tabs', tabs);
 
       if (lotusTab) {
         var hash = '#/tickets/' + id;
@@ -21,6 +20,5 @@ function openTicketInZendesk(tab) {
 }
 
 chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
-  console.log('onBeforeNavigate details', details);
   openTicketInZendesk({ id: details.tabId, url: details.url });
 }, { url: [ { hostSuffix: 'zendesk.com' } ] });
