9fb7cd6209a500bc0a2d25e86d896243da7051c3
Igor Justino <dadah89@gmail.com>
2013-04-02 02:47:53 -0700

    webNavigation.onBeforeNavigate instead of tabs.onUpdated (way faster)

--- background.js
+++ background.js
@@ -1,23 +1,18 @@
-  // "browser_action": {
-  //   "default_name":  "New Zendesk Tabs"
-  // },
-
 function openTicketInZendesk(tab) {
-  // console.log('tab before match', tab);
+  console.log('tab before match', tab);
   var match = tab.url.match(/^https?:\/\/(.*)\.zendesk.com\/tickets\/(\d+)$/);
   if (match) {
-    var domain = match[1],
+    var domain  = match[1],
         pattern = '*://' + domain + '.zendesk.com/agent/*',
-        id = match[2];
+        id      = match[2];
     // console.log('domain', domain, 'pattern', pattern);
-    // chrome.browserAction.setBadgeText({ "text": id, tabId: tab.id });
     chrome.tabs.query({ url: pattern }, function(tabs) {
       var lotusTab = tabs[0];
       // console.log('tabs', tabs);
 
       if (lotusTab) {
-        var newUrl = 'https://' + domain + '.zendesk.com/agent/#/tickets/' + id;
-        chrome.tabs.executeScript(lotusTab.id, { code: 'window.location.hash = "#/tickets/' + id + '";' })
+        var hash = '#/tickets/' + id;
+        chrome.tabs.executeScript(lotusTab.id, { code: 'window.location.hash = "' + hash + '";' })
         chrome.tabs.update(lotusTab.id, { active: true });
         chrome.tabs.remove(tab.id);
       }
@@ -25,8 +20,7 @@ function openTicketInZendesk(tab) {
   }
 }
 
-chrome.tabs.onUpdated.addListener(function(tabId, props, tab) {
-  if (props.status == "complete") {
-    openTicketInZendesk(tab);
-  }
-});
+chrome.webNavigation.onBeforeNavigate.addListener(function(details) {
+  console.log('onBeforeNavigate details', details);
+  openTicketInZendesk({ id: details.tabId, url: details.url });
+}, { url: [ { hostSuffix: 'zendesk.com' } ] });
